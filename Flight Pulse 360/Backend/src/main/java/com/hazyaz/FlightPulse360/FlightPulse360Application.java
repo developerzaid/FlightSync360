82cafc41cb537475f80d66ef5bcd42fcbdbe1753
zaid <developerzaid@gmail.com>
2025-11-13 17:24:28 +0000

    Initial Commit

---
+++ Flight Pulse 360/Backend/src/main/java/com/hazyaz/FlightPulse360/FlightPulse360Application.java
@@ -0,0 +1,13 @@
+package com.hazyaz.FlightPulse360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPulse360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPulse360Application.class, args);
+	}
+
+}
---
+++ Flight Pulse 360/Backend/src/test/java/com/hazyaz/FlightPulse360/FlightPulse360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazyaz.FlightPulse360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPulse360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
